a6f1b4152ace3872d0b1140a8ffe711e630d5039
haakongar <garfjellhaakon@gmail.com>
2024-03-01 15:38:16 -0800

    Fixed routing

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -1,46 +1,30 @@
 <script>
-  import { onMount } from 'svelte';
+  import Home from './Home.svelte';
   import Page1 from './Page1.svelte';
   import Page2 from './Page2.svelte';
 
-  let currentPage = '';
+  let currentPage = 'home';
 
-const navigate = (page) => {
-    if (typeof window !== 'undefined') {
-        window.history.pushState({ page }, '', `/${page}`);
-    }
+  const navigate = (page) => {
     currentPage = page;
-};
-
-onMount(() => {
-    if (typeof window !== 'undefined') {
-        const path = window.location.pathname;
-        currentPage = path.substring(1);
-    }
-});
-
-if (typeof window !== 'undefined') {
-    window.onpopstate = (event) => {
-        if (event.state && event.state.page) {
-            currentPage = event.state.page;
-        }
-    };
-}
+  };
 </script>
 
 <nav>
-<ul>
-    <li><a href="#" on:click={() => navigate('page1')}>Page 1</a></li>
-    <li><a href="#" on:click={() => navigate('page2')}>Page 2</a></li>
+  <ul>
+    <li><a href="#" on:click|preventDefault={() => navigate('home')}>Home</a></li>
+    <li><a href="#" on:click|preventDefault={() => navigate('page1')}>Page 1</a></li>
+    <li><a href="#" on:click|preventDefault={() => navigate('page2')}>Page 2</a></li>
     <!-- Add more pages as needed -->
-</ul>
+  </ul>
 </nav>
 
-{#if currentPage === 'page1'}
-<Page1 />
+{#if currentPage === 'home'}
+  <Home />
+{:else if currentPage === 'page1'}
+  <Page1 />
 {:else if currentPage === 'page2'}
-<Page2 />
+  <Page2 />
 {:else}
-<!-- Handle 404 or default page here -->
+  <p>Page not found</p>
 {/if}
-
---
+++ src/components/Home.svelte
@@ -0,0 +1,7 @@
+<script>
+    // You can add logic specific to Page1 here
+</script>
+
+<div>
+    <h1>Home</h1>
+</div>
